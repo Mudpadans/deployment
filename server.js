a1dee9ffb5cd4548825d40bfbd72e4a59a4c4e10
Mudpadans <philmodel.101@gmail.com>
2023-05-22 11:58:23 -0400

    port name change

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express()
 
 app.use(express.static(`${__dirname}/public`))
 
-app.listen(5000, () => console.log(`server running on 5000`))
+app.listen(4956, () => console.log(`server running on 4956`))
 
