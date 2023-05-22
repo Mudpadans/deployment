67152b966f503427113e8627abc97807f2ebc1e3
Mudpadans <philmodel.101@gmail.com>
2023-05-22 11:20:02 -0400

    link

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express()
 
 app.use(express.static(`${__dirname}/public`))
 
-app.listen(4000, () => console.log(`server running on 4000`))
+app.listen(5000, () => console.log(`server running on 5000`))
 
