c6cfde57a0b63961b870076645169d235d9836e1
Mudpadans <philmodel.101@gmail.com>
2023-05-23 22:25:35 -0400

    sequelize

--- server.js
+++ server.js
@@ -7,15 +7,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const sequelize = new Sequelize(CONNECTION_STRING, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    },
-});
-
 app.use(express.static(path.join(__dirname, "/public")))
 
 app.listen(4956, () => console.log(`server running on 4956`))
