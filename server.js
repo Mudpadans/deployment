a1cd192d8c61f69b4c8d3a5c103c095d8a56ac2b
Mudpadans <philmodel.101@gmail.com>
2023-05-24 13:00:46 -0400

    rollbar

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, "/public")))
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '32e927c44b6742288668c16a45e1074b',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello World')
+
 app.listen(4956, () => console.log(`server running on 4956`))
 
 // const sequelize = new Sequelize(CONNECTION_STRING, {
