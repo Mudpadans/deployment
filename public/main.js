45b9570e72a42ec2768a1df304c3e800682eb89d
Mudpadans <philmodel.101@gmail.com>
2023-05-27 12:27:21 -0400

    calculator

--- public/main.js
+++ public/main.js
@@ -1,22 +1,35 @@
 const testButton = document.getElementById("test-btn")
+const popup = document.getElementById("popupBox")
+const link = document.querySelector('a')
+const hideButton = document.getElementById('hide-btn')
 
 const showPopup = () => {
-    var popup = document.getElementById("popupBox")
-    popup.style.display = 'block'
-    rollbar.log("popup works")
+    // popup.style.display = 'block'
+    axios.get("http://localhost:4956/api/showPopUp")
+    .then(res => {
+      if (res.data.shouldShowPopup) {
+        popup.style.display = 'block';
+      }
+    })
 } 
 
 const hidePopup = () => {
-    var popup = document.getElementById("popupBox")
-    popup.style.display = 'none'
-}
+    // popup.style.display = 'none'
+    axios.get("http://localhost:4956/api/hidePopup")
+    .then(res => {
+      if (!res.data.shouldShowPopup) {
+      popup.style.display = 'none';
+      }
+    })
+  }
 
 const testMessage = () => {
-  axios.get("/api/testMessage")
+  axios.get("http://localhost:4956/api/testMessage")
     .then(res => {
       alert(res.data)
     })
 }
 
+link.addEventListener('click', showPopup)
+hideButton.addEventListener('click', hidePopup)
 testButton.addEventListener('click', testMessage)
-
--- server.js
+++ server.js
@@ -9,8 +9,6 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, "/public")))
 
-const {testMessage} = require('./controller.js')
-
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
   accessToken: '32e927c44b6742288668c16a45e1074b',
@@ -20,36 +18,30 @@ var rollbar = new Rollbar({
 
 rollbar.log('Hello World')
 
+app.get('/api/showPopup', (req, res) => {
+    const shouldShowPopup = true;
+    res.status(200).send({ shouldShowPopup });
+    rollbar.info("popup shows up")
+})
+
+app.get('/api/hidePopup', (req, res) => {
+    const shouldShowPopup = false;
+    res.status(200).send({ shouldShowPopup });
+    rollbar.info("popup hidden")
+})
+
 app.get('/api/testMessage', (req, res) => {
     res.status(200).send("it's a miracle!!")
     rollbar.info("it's a miracle!!")
 })
 
-app.get('/public', (req, res) => {
-    try {
-        rollbar.info('Front-end function called: showPopup');
-        res.send('Front-end function called');
-    } catch (error) {
-        console.log(error);
-        rollbar.error('Error occurred while calling front-end function: showPopup');
-        res.status(500).send('Error occurred');
-    }
-});
-
-// try {
-//     mysteryFunction();
-// } catch (error) {
-//     rollbar.error('non-existent function')
-//     console.error(error)
-// }
+try {
+    mysteryFunction();
+    rollbar.warning("unused function")
+} catch (error) {
+    rollbar.error('non-existent function')
+    console.error(error)
+}
 
 app.listen(4956, () => console.log(`server running on 4956`))
 
-// const sequelize = new Sequelize(CONNECTION_STRING, {
-//     dialect: 'postgres',
-//     dialectOptions: {
-//         ssl: {
-//             rejectUnauthorized: false,
-//         },
-//     },
-// });
